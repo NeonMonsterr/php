3f432d2bacf5991b42df62d3cf59374e626eb849
NeonMonsterr <ahmedhummer56@gmail.com>
2025-09-04 14:24:47 +0200

    safe removing

--- BioMarakby/resources/views/partials/back-to-home.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="mt-6 text-left">
-    <a href="{{ route('dashboard') }}"
-       class="inline-flex items-center px-4 py-2 bg-gray-100 text-gray-800 font-semibold rounded-md hover:bg-gray-200 transition duration-200 ease-in-out">
-        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
-        </svg>
-        Back to Home
-    </a>
-</div>
